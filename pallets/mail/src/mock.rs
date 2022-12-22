2750aafe10985dc74a3b5c76e77e0a741a3a60b0
shiyivei <shiyivei@outlook.com>
2022-12-22 09:26:53 +0800

    build a mock test environment

--- pallets/mail/src/mock.rs
+++ pallets/mail/src/mock.rs
@@ -1,11 +1,16 @@
 use crate as pallet_mail;
-use frame_support::traits::{ConstU16, ConstU64};
+use frame_support::{
+	parameter_types,
+	traits::{ConstU16, ConstU64},
+};
 use frame_system as system;
 use sp_core::H256;
 use sp_runtime::{
 	testing::Header,
 	traits::{BlakeTwo256, IdentityLookup},
 };
+
+use sp_core::sr25519::{Public, Signature};
 
 type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
 type Block = frame_system::mocking::MockBlock<Test>;
@@ -18,9 +23,14 @@ frame_support::construct_runtime!(
 		UncheckedExtrinsic = UncheckedExtrinsic,
 	{
 		System: frame_system,
-		Mail: pallet_mail,
+		MailModule: pallet_mail,
 	}
 );
+
+use sp_runtime::{
+	testing::TestXt,
+	traits::{Extrinsic as ExtrinsicT, IdentifyAccount, Verify},
+};
 
 impl system::Config for Test {
 	type BaseCallFilter = frame_support::traits::Everything;
@@ -33,7 +43,7 @@ impl system::Config for Test {
 	type BlockNumber = u64;
 	type Hash = H256;
 	type Hashing = BlakeTwo256;
-	type AccountId = u64;
+	type AccountId = sp_core::sr25519::Public;
 	type Lookup = IdentityLookup<Self::AccountId>;
 	type Header = Header;
 	type RuntimeEvent = RuntimeEvent;
@@ -49,8 +59,44 @@ impl system::Config for Test {
 	type MaxConsumers = frame_support::traits::ConstU32<16>;
 }
 
+parameter_types! {
+	pub const UnsignedPriority: u64 = 1;
+}
+
+type Extrinsic = TestXt<RuntimeCall, ()>;
+type AccountId = <<Signature as Verify>::Signer as IdentifyAccount>::AccountId;
+
 impl pallet_mail::Config for Test {
 	type RuntimeEvent = RuntimeEvent;
+	type AuthorityId = pallet_mail::crypto::TestAuthId;
+	type UnsignedPriority = UnsignedPriority;
+}
+
+impl frame_system::offchain::SigningTypes for Test {
+	type Public = <Signature as Verify>::Signer;
+	type Signature = Signature;
+}
+
+impl<C> frame_system::offchain::SendTransactionTypes<C> for Test
+where
+	RuntimeCall: From<C>,
+{
+	type Extrinsic = UncheckedExtrinsic;
+	type OverarchingCall = RuntimeCall;
+}
+
+impl<LocalCall> frame_system::offchain::CreateSignedTransaction<LocalCall> for Test
+where
+	RuntimeCall: From<LocalCall>,
+{
+	fn create_transaction<C: frame_system::offchain::AppCrypto<Self::Public, Self::Signature>>(
+		call: RuntimeCall,
+		_public: <Signature as Verify>::Signer,
+		_account: AccountId,
+		nonce: u64,
+	) -> Option<(RuntimeCall, <Extrinsic as ExtrinsicT>::SignaturePayload)> {
+		Some((call, (nonce, ())))
+	}
 }
 
 // Build genesis storage according to the mock runtime.
--- pallets/mail/src/tests.rs
+++ pallets/mail/src/tests.rs
@@ -1,8 +1,34 @@
+use super::*;
 use crate::{mock::*, Error};
+
 use frame_support::{assert_noop, assert_ok};
+
+use mock::RuntimeEvent as Event;
+use sp_core::sr25519::Public;
 
 #[test]
 fn it_works_for_bind_address() {
+	new_test_ext().execute_with(|| {
+		let signer = Public::from_raw([0; 32]);
+		let pmail_address: BoundedVec<u8, ConstU32<128>> = vec![1, 2, 3].try_into().unwrap();
+
+		// assert
+		assert_ok!(MailModule::bind_address(RuntimeOrigin::signed(signer), pmail_address));
+
+		// assert transfer limit owner
+		assert_eq!(MailMap::<Test>::get(signer), Some(pmail_address));
+		assert_eq!(OwnerMap::<Test>::get(pmail_address), Some(signer));
+
+		// assert successful events
+		System::assert_has_event(Event::MailModule(crate::Event::AddressBound(
+			signer,
+			pmail_address,
+		)));
+	});
+}
+
+#[test]
+fn it_works_for_set_alias() {
 	new_test_ext().execute_with(|| {
 		// Dispatch a signed extrinsic.
 		// assert_ok!(Mail::bind_address(RuntimeOrigin::signed(1), 42));
@@ -11,7 +37,16 @@ fn it_works_for_bind_address() {
 }
 
 #[test]
-fn correct_error_for_none_value() {
+fn it_works_for_send_mail() {
+	new_test_ext().execute_with(|| {
+		// Dispatch a signed extrinsic.
+		// assert_ok!(Mail::bind_address(RuntimeOrigin::signed(1), 42));
+		// Read pallet storage and assert an expected result.
+	});
+}
+
+#[test]
+fn it_will_fail_when_bind_address_duplicate() {
 	new_test_ext().execute_with(|| {
 		// Ensure the expected error is thrown when no value is present.
 		// assert_noop!(Mail::bind_address(RuntimeOrigin::signed(1)), Error::<Test>::NoneValue);
